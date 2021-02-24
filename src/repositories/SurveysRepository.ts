3db3634e0fa13343f389d17a3fb4141825ed606e
jhongomes <jhonatangomes859@gmail.com>
2021-02-24 18:50:16 -0400

    Adicionando create surveys

---
+++ src/repositories/SurveysRepository.ts
@@ -0,0 +1,13 @@
+import { Repository } from "typeorm";
+import { EntityRepository } from "typeorm";
+import Survey from "../models/Survey";
+
+
+
+@EntityRepository(Survey)
+class SurveysRepository extends Repository <Survey>{
+
+}
+
+
+export default SurveysRepository;
