f0670c54f5a2ff8b2b988ff5e70135c476443ae3
jhongomes <jhonatangomes859@gmail.com>
2021-02-26 19:33:23 -0400

    Adicionando model  SurveyUsers

---
+++ src/models/SurveyUser.ts
@@ -0,0 +1,34 @@
+import { CreateDateColumn, Entity, Column, PrimaryColumn } from "typeorm";
+import { v4 as uuid } from 'uuid';
+
+
+@Entity("surveys_users")
+class SurveyUser {
+
+@PrimaryColumn()
+readonly id: string;
+
+@Column()
+user_id: string;
+
+@Column()
+survey_id: string;
+
+@Column()
+value: number;
+
+@CreateDateColumn()
+created_at: Date;
+
+@CreateDateColumn()
+updated_at: Date;
+
+constructor(){
+    if(!this.id){
+        this.id = uuid()
+    }
+}
+
+
+}
+export default SurveyUser;
