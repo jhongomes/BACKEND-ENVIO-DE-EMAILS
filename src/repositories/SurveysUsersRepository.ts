6f3adb1853618b5739ee225ecdae9baa0169cd6f
jhongomes <jhonatangomes859@gmail.com>
2021-02-26 22:57:00 -0400

    Adicionando repository SurveysUsersRepository

---
+++ src/repositories/SurveysUsersRepository.ts
@@ -0,0 +1,12 @@
+import { Entity } from "typeorm";
+import { EntityRepository } from "typeorm";
+import { Repository } from "typeorm";
+import SurveyUser from "../models/SurveyUser";
+
+
+@EntityRepository(SurveyUser)
+class SurveysUsersRepository  extends Repository<SurveyUser> {
+
+}
+
+export default SurveysUsersRepository;
